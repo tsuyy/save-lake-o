5deee14be8ad3f0cf233e9d203330607ccde4a73
yvntsu <yvonnnetsu@gmail.com>
2020-10-20 01:21:04 -0400

    update responsive nav

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,3 +1,41 @@
 $(document).ready(function() {
+    // toggle active on click
+    $(document).on('click', '.nav-items a', function() {
+        $('.nav-items a').removeClass('active');
+        $(this).addClass('active');
+    });
 
+    $('body').css('padding-top', $('.navbar').outerHeight() + 'px')
+
+    // detect scroll top or down
+    if ($('.smart-scroll').length > 0) { // check if element exists
+        var last_scroll_top = 0;
+        $(window).on('scroll', function() {
+            scroll_top = $(this).scrollTop();
+            if(scroll_top < last_scroll_top) {
+                $('.smart-scroll').removeClass('scrolled-down').addClass('scrolled-up');
+            }
+            else {
+                $('.smart-scroll').removeClass('scrolled-up').addClass('scrolled-down');
+            }
+            last_scroll_top = scroll_top;
+        });
+    }
 });
+
+$(window)
+    .scroll(function() {
+            const scrollDistance = $(window).scrollTop() + 150;
+
+            // Assign active class to nav links while scolling
+            $('.page-section').each(function(i) {
+                    if ($(this).position().top <= scrollDistance) {
+                            $('.nav-items a.active').removeClass('active');
+                            $('.nav-items a')
+                                    .eq(i)
+                                    .addClass('active');
+                    }
+            });
+    })
+    .scroll();
+
