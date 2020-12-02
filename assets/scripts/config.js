239e3141b650233b103ceb0adbf069d9cfd8f69d
yvntsu <yvonnnetsu@gmail.com>
2020-12-01 17:36:25 -0800

    updates agr cards

--- assets/scripts/config.js
+++ assets/scripts/config.js
@@ -176,7 +176,7 @@ var config = {
             id: '06',
             title: '1980s – 2010s',
             image: '',
-            description: 'Major algal blooms throughout the 1980s and 1990s caused as a result of legacy nutrients trapped in the earth, and sewerage through the canal systems prompted the creation of a series of plans. This includes the Surface Water Improvement and Management Plan in 1989, the Florida Harmful Algal Bloom Task Force (HABTF ) in 1996, the Lake Okeechobee Protection Act in 2000, and the Northern Everglades and Estuaries Protection Program in 2007.</br>By 2016, Harmful algae blooms cover 33 square miles of Lake Okeechobee. In 2018 and 2019 more harmful algal blooms spur a state of emergency. Blooms spread from Lake Okeechobee through the canal systems and across the state through the Okeechobee Waterway leading out to the Atlantic Ocean on the east coast and the Gulf of Mexico to the west.This results  in the Everglades Agricultural Area Storage Reservoir Project by the U.S. Army Corps of Engineers to reduce the risk from harmful algal blooms.',
+            description: 'Major algal blooms throughout the 1980s and 1990s caused as a result of legacy nutrients trapped in the earth, and sewerage through the canal systems prompted the creation of a series of plans. This includes the Surface Water Improvement and Management Plan in 1989, the Florida Harmful Algal Bloom Task Force (HABTF ) in 1996, the Lake Okeechobee Protection Act in 2000, and the Northern Everglades and Estuaries Protection Program in 2007.',
             location: {
                 center: [ -81.29489, 26.92825],
                 zoom: 8.6,
@@ -198,6 +198,30 @@ var config = {
         },
         {
             id: '07',
+            title: '',
+            image: '',
+            description: 'By 2016, Harmful algae blooms cover 33 square miles of Lake Okeechobee. In 2018 and 2019 more harmful algal blooms spur a state of emergency. Blooms spread from Lake Okeechobee through the canal systems and across the state through the Okeechobee Waterway leading out to the Atlantic Ocean on the east coast and the Gulf of Mexico to the west.This results in the Everglades Agricultural Area Storage Reservoir Project by the U.S. Army Corps of Engineers to reduce the risk from harmful algal blooms.',
+            location: {
+                center: [ -81.29489, 26.92825],
+                zoom: 8.6,
+                pitch: 0,
+                bearing: 0
+            },
+            onChapterEnter: [
+                {
+                    layer: '2018-algal-bloom',
+                    opacity: 0.25
+                }
+            ],
+            onChapterExit: [
+                {
+                    layer: '2018-algal-bloom',
+                    opacity: 0
+                }
+            ]
+        },
+        {
+            id: '08',
             title: '',
             image: './assets/img/water-farm.jpg',
             description: 'The Caulkins Water Farm, a public-private partnership, started in 2014 as a temporary solution to nutrient runoff by holding water to reduce harmful levels of nutrients flowing into the St. Lucie Estuary. Today, the  3,200-acre water farm is part  of the permanent solution, holding 60,000 acre-feet of water per year.',
@@ -221,7 +245,7 @@ var config = {
             ]
         },
         {
-            id: '08',
+            id: '09',
             title: 'Today',
             image: '',
             description: 'Today, Lake Okeechobee is about 730 square miles and 9 feet deep. About 60% of the lake’s water flows in from rivers and tributaries in the north, while 40% comes from rainfall. The lake is managed via a canal system on both the federal and state level. From the water flowing in from rivers and tributaries 78% of the nutrient runoff is coming from agricultural lands, 14% from natural lands, and 8% from developed lands including residential areas.',
@@ -253,7 +277,7 @@ var config = {
             ]
         },
         {
-            id: '09',
+            id: '10',
             title: 'Population growth',
             image: './assets/img/population-growth.png',
             description: 'The 2019 Florida Census estimates the population in the five Lake Okeechobee counties (Glades, Hendry, Martin, Okeechobee and Palm Beach) have grown 80-97% since 1930.',
@@ -272,7 +296,6 @@ var config = {
                     layer: 'county-label',
                     opacity: 1
                 }
-
             ],
             onChapterExit: [
                 {
@@ -286,10 +309,42 @@ var config = {
             ]
         },
         {
-            id: '10',
+            id: '11',
             title: '',
             image: '',
             description: 'Florida’s top agricultural industries include cattle/calf livestock with many of the ranches located north of Lake Okeechobee, citrus with many of the farms to the southwest, and sugarcane with many of the farms south and southwest of the lake.',
+            location: {
+                center: [-81.29489, 26.92825],
+                zoom: 8.7,
+                pitch: 0,
+                bearing: 0
+            },
+            onChapterEnter: [
+                {
+                    layer: 'county-outline',
+                    opacity: 0.9
+                },
+                {
+                    layer: 'county-label',
+                    opacity: 1
+                }
+            ],
+            onChapterExit: [
+                {
+                    layer: 'county-label',
+                    opacity: 0
+                },
+                {
+                    layer: 'county-outline',
+                    opacity: 0
+                }
+            ]
+        },        
+        {
+            id: '12',
+            title: 'Truck and Row Crops',
+            image: './assets/img/veg.jpg',
+            description: 'Palm Beach County has the highest percentage of irregated land used at 17% for the growing vegetables.',
             location: {
                 center: [-81.29489, 26.92825],
                 zoom: 8.7,
@@ -318,74 +373,74 @@ var config = {
             ]
         },
         {
-            id: '11',
-            title: '',
-            image: '',
-            description: '',
-            location: {
-                center: [ -81.29489, 26.92825],
-                zoom: 8.7,
-                pitch: 0,
-                bearing: 0
-            },
-            onChapterEnter: [
-                {
-                    layer: 'citrus',
-                    opacity: 1
-                },
-                {
-                    layer: 'citrus-label',
-                    opacity: 0.9
-                }
-            ],
-            onChapterExit: [
-                {
-                    layer: 'citrus',
-                    opacity: 0
-                },
-                {
-                    layer: 'citrus-label',
-                    opacity: 0
-                }
-            ]
-        },
-        {
-            id: '12',
-            title: '',
-            image: '',
-            description: '',
-            location: {
-                center: [ -81.29489, 26.92825],
-                zoom: 8.7,
-                pitch: 0,
-                bearing: 0
-            },
-            onChapterEnter: [
-                {
-                    layer: 'cattle',
-                    opacity: 1
-                },
-                {
-                    layer: 'cattle-label',
-                    opacity: 0.9
-                }
-            ],
-            onChapterExit: [
-                {
-                    layer: 'cattle',
-                    opacity: 0
-                },
-                {
-                    layer: 'cattle-label',
-                    opacity: 0
-                }
-            ]
-        },
-        {
             id: '13',
-            title: '',
-            image: '',
-            description: '',
+            title: 'Citrus Farms',
+            image: './assets/img/citrus.jpg',
+            description: 'Hendry County has the highest percentage of irrigated land used at 37% for growing citrus, primarily oranges.',
+            location: {
+                center: [ -81.29489, 26.92825],
+                zoom: 8.7,
+                pitch: 0,
+                bearing: 0
+            },
+            onChapterEnter: [
+                {
+                    layer: 'citrus',
+                    opacity: 1
+                },
+                {
+                    layer: 'citrus-label',
+                    opacity: 0.9
+                }
+            ],
+            onChapterExit: [
+                {
+                    layer: 'citrus',
+                    opacity: 0
+                },
+                {
+                    layer: 'citrus-label',
+                    opacity: 0
+                }
+            ]
+        },
+        {
+            id: '14',
+            title: 'Calf/Cattle and Dairy Farms',
+            image: './assets/img/cattle.jpg',
+            description: 'Cattle are raised in all five counties with the highest percentage of land used in Okeechobee County at 45%.',
+            location: {
+                center: [ -81.29489, 26.92825],
+                zoom: 8.7,
+                pitch: 0,
+                bearing: 0
+            },
+            onChapterEnter: [
+                {
+                    layer: 'cattle',
+                    opacity: 1
+                },
+                {
+                    layer: 'cattle-label',
+                    opacity: 0.9
+                }
+            ],
+            onChapterExit: [
+                {
+                    layer: 'cattle',
+                    opacity: 0
+                },
+                {
+                    layer: 'cattle-label',
+                    opacity: 0
+                }
+            ]
+        },
+        {
+            id: '15',
+            title: 'Sugarcane Farms',
+            image: './assets/img/sugarcane.jpg',
+            description: 'Sugarcane production is high in four of the five counties with 74% of land in Palm Beach County used for growing.',
             location: {
                 center: [ -81.29489, 26.92825],
                 zoom: 8.7,
@@ -414,7 +469,7 @@ var config = {
             ]
         },
         {
-            id: '14',
+            id: '16',
             title: '',
             image: './assets/img/lake-o.jpg',
             description: 'The future of Lake Okeechobee is hopeful. Communities are coming together to contribute towards the health of our beautiful Lake Okeechobee. Each of us can be a part of the solution.',
